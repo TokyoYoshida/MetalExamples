51246a5b9f8691dc2fb0543a9c3b8c37a5e30660
tokyoyoshida <yoshidaforpublic@gmail.com>
2021-06-03 19:27:23 +0900

    add navigation

--- MetalExamples/ContentView.swift
+++ MetalExamples/ContentView.swift
@@ -9,8 +9,12 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+             NavigationLink(destination: SimpleView()) {
+                 Text("Simple")
+                     .padding()
+             }
+         }
     }
 }
 
---
+++ MetalExamples/Samples/Simple/Simple.swift
@@ -0,0 +1,15 @@
+//
+//  Simple.swift
+//  MetalExamples
+//
+//  Created by TokyoYoshida on 2021/06/03.
+//
+
+import SwiftUI
+
+struct SimpleView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
