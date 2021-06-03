71b804c6c1559fa0b8e2ce0d6c5d49f27af04d8a
tokyoyoshida <yoshidaforpublic@gmail.com>
2021-06-03 19:21:31 +0900

    first commit

---
+++ MetalExamples/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MetalExamples
+//
+//  Created by TokyoYoshida on 2021/06/03.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MetalExamples/MetalExamplesApp.swift
@@ -0,0 +1,17 @@
+//
+//  MetalExamplesApp.swift
+//  MetalExamples
+//
+//  Created by TokyoYoshida on 2021/06/03.
+//
+
+import SwiftUI
+
+@main
+struct MetalExamplesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
