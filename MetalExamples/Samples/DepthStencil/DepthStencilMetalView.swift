ab856b1c35ad77ce8e88fdb15f207aa5c438f8db
tokyoyoshida <yoshidaforpublic@gmail.com>
2021-08-01 15:56:40 +0900

    add DepthStencil

---
+++ MetalExamples/Samples/DepthStencil/DepthStencilMetalView.swift
@@ -0,0 +1,214 @@
+//
+//  MetalView.swift
+//  MetalExamples
+//
+//  Created by TokyoYoshida on 2021/06/04.
+//
+
+import SwiftUI
+import MetalKit
+
+struct DepthStencilMetalView: UIViewRepresentable {
+    typealias UIViewType = MTKView
+    let mtkView = MTKView()
+
+    func makeCoordinator() -> Coordinator {
+        Coordinator(self)
+    }
+    func makeUIView(context: Context) -> MTKView {
+        mtkView.delegate = context.coordinator
+        mtkView.preferredFramesPerSecond = 60
+        if let metalDevice = MTLCreateSystemDefaultDevice() {
+            mtkView.device = metalDevice
+        }
+        mtkView.framebufferOnly = false
+        mtkView.clearColor = MTLClearColor(red: 0, green: 0, blue: 0, alpha: 0)
+        mtkView.drawableSize = mtkView.frame.size
+        mtkView.colorPixelFormat = .bgra8Unorm_srgb
+        return mtkView
+    }
+    func updateUIView(_ uiView: MTKView, context: Context) {
+    }
+    class Coordinator : NSObject, MTKViewDelegate {
+        static let numberOfParticles = 100_000
+        static let maxBuffers = 3
+        lazy var indexes = makeParticleIndexes()
+        var parent: DepthStencilMetalView
+        var metalDevice: MTLDevice!
+        var metalCommandQueue: MTLCommandQueue!
+        var renderPipeline: MTLRenderPipelineState!
+        var particleBuffers:[MTLBuffer] = []
+        lazy var indexBuffer: MTLBuffer = metalDevice.makeBuffer(length: MemoryLayout<UInt32>.stride * indexes.count, options: .storageModeShared)!
+        var renderPassDescriptor: MTLRenderPassDescriptor = MTLRenderPassDescriptor()
+        var uniforms: Uniforms!
+        var preferredFramesTime: Float!
+        let semaphore = DispatchSemaphore(value: Coordinator.maxBuffers)
+        var currentBufferIndex = 0
+        var beforeBufferIndex: Int {
+            currentBufferIndex == 0 ? Coordinator.maxBuffers - 1 : currentBufferIndex - 1
+        }
+
+        init(_ parent: DepthStencilMetalView) {
+            func buildPipeline() {
+                guard let library = self.metalDevice.makeDefaultLibrary() else {fatalError()}
+                let descriptor = MTLRenderPipelineDescriptor()
+                descriptor.vertexFunction = library.makeFunction(name: "storedParticleVertexShader")
+                descriptor.fragmentFunction = library.makeFunction(name: "storedParticleFragmentShader")
+                descriptor.colorAttachments[0].pixelFormat = .bgra8Unorm_srgb
+                renderPipeline = try! self.metalDevice.makeRenderPipelineState(descriptor: descriptor)
+            }
+            func initUniform() {
+                uniforms = Uniforms(time: Float(0.0), aspectRatio: Float(0.0), touch: SIMD2<Float>(), resolution: SIMD4<Float>())
+                uniforms.aspectRatio = Float(parent.mtkView.frame.size.width / parent.mtkView.frame.size.height)
+                preferredFramesTime = 1.0 / Float(parent.mtkView.preferredFramesPerSecond)
+            }
+            func initParticles() {
+                func allocBuffer() -> [MTLBuffer] {
+                    var buffers:[MTLBuffer] = []
+                    for _ in 0..<Coordinator.maxBuffers {
+                        let length = MemoryLayout<Particle>.stride * Coordinator.numberOfParticles
+                        guard let buffer = metalDevice.makeBuffer(length: length, options: .storageModeShared) else {
+                            fatalError("Cannot make particle buffer.")
+                        }
+                        buffers.append(buffer)
+                    }
+                    return buffers
+                }
+                func initParticlePosition(_ particleBuffer: MTLBuffer) {
+                    let particles = makeParticlePositions()
+                    self.particleBuffers[0].contents().copyMemory(from: particles, byteCount: MemoryLayout<Particle>.stride * particles.count)
+                }
+                func initIndexBuffer() {
+                    let indexes = makeParticleIndexes()
+                    self.indexBuffer.contents().copyMemory(from: indexes, byteCount: MemoryLayout<UInt32>.stride * indexes.count)
+                }
+                particleBuffers = allocBuffer()
+                initParticlePosition(particleBuffers[0])
+                initIndexBuffer()
+            }
+            self.parent = parent
+            if let metalDevice = MTLCreateSystemDefaultDevice() {
+                self.metalDevice = metalDevice
+            }
+            self.metalCommandQueue = metalDevice.makeCommandQueue()!
+            super.init()
+            buildPipeline()
+            initUniform()
+            initParticles()
+        }
+        
+        func makeRandomPosition() -> Particle {
+            var particle = Particle()
+            particle.position.x = Float.random(in: -1...1)
+            particle.position.y = Float.random(in: -1...1)
+            return particle
+        }
+        
+        func calcLayoutParams() -> (deltaX: UInt32, deltaY: UInt32, spacing: Float) {
+            let width: Float = 2
+            let height: Float = 2
+            let gridArea = width * height
+            let spacing = sqrt(gridArea / Float(Coordinator.numberOfParticles))
+            let deltaX = UInt32(round(width / spacing))
+            let deltaY = UInt32(round(height / spacing))
+            
+            return (deltaX: deltaX, deltaY: deltaY, spacing: spacing)
+        }
+
+        func makeParticlePositions() -> [Particle] {
+            var particles: [Particle] = []
+
+            let (deltaX, deltaY, spacing) = calcLayoutParams()
+            
+            for gridY in 0 ..< deltaY {
+                let alternatingOffsetX: Float = -1
+                for gridX in 0 ..< deltaX {
+                    let position = SIMD2<Float>(alternatingOffsetX + (Float(gridX) + 0.5) * spacing, (Float(gridY) + 0.5) * spacing - 1)
+                    let particle = Particle(position: position)
+                    particles.append(particle)
+                }
+            }
+            
+            return particles
+        }
+        
+        func makeParticleIndexes() -> [UInt32] {
+            let (deltaX, deltaY, _) = calcLayoutParams()
+
+            var indexes: [UInt32] = []
+            for gridY in 0 ..< deltaY - 1 {
+                for gridX in 0 ..< deltaX - 1 {
+                    let topLeft = gridY * deltaY + gridX
+                    let topRight = topLeft + 1
+                    let bottomLeft = topLeft + deltaY
+                    let bottomRight = bottomLeft + 1
+                    let leftTriangle = [topLeft, bottomLeft, topRight]
+                    let rightTriangle = [topRight, bottomRight, bottomLeft]
+                    indexes += (leftTriangle + rightTriangle)
+                }
+            }
+            
+            return indexes
+        }
+
+        func mtkView(_ view: MTKView, drawableSizeWillChange size: CGSize) {
+        }
+        func draw(in view: MTKView) {
+            func calcParticlePostion() {
+                let p = particleBuffers[currentBufferIndex].contents()
+                let b = particleBuffers[beforeBufferIndex].contents()
+                let stride = MemoryLayout<Particle>.stride
+                for i in 0..<Coordinator.numberOfParticles {
+                    var particle = b.load(fromByteOffset: i*stride, as: Particle.self)
+                    if particle.position.y > -1 {
+                        particle.position.y -= 0.01
+                    } else {
+                        particle.position.y += 2 - 0.01
+                    }
+                    p.storeBytes(of: particle,toByteOffset: i*stride,  as: Particle.self)
+                }
+            }
+            guard let drawable = view.currentDrawable else {return}
+            
+            semaphore.wait()
+            let commandBuffer = metalCommandQueue.makeCommandBuffer()!
+            
+            currentBufferIndex = (currentBufferIndex + 1) % Coordinator.maxBuffers
+            calcParticlePostion()
+            
+            renderPassDescriptor.colorAttachments[0].texture = drawable.texture
+            renderPassDescriptor.colorAttachments[0].loadAction = .clear
+            renderPassDescriptor.colorAttachments[0].clearColor = MTLClearColorMake(0.8, 0.7, 0.1, 1.0)
+
+            let renderEncoder = commandBuffer.makeRenderCommandEncoder(descriptor: renderPassDescriptor)!
+
+            guard let renderPipeline = renderPipeline else {fatalError()}
+
+            
+            renderEncoder.setRenderPipelineState(renderPipeline)
+            uniforms.time += preferredFramesTime
+
+            renderEncoder.setVertexBuffer(particleBuffers[currentBufferIndex], offset: 0, index: 0)
+            
+            renderEncoder.setVertexBytes(&uniforms, length: MemoryLayout<Uniforms>.stride, index: 2)            
+
+//            renderEncoder.drawPrimitives(type: .point, vertexStart: 0, vertexCount: Coordinator.numberOfParticles)
+            
+            renderEncoder.drawIndexedPrimitives(type: .triangle,
+                                                indexCount: indexes.count,
+                                                indexType: .uint32,
+                                                indexBuffer: indexBuffer,
+                                                indexBufferOffset: 0,
+                                                instanceCount: 1)
+
+            renderEncoder.endEncoding()
+            
+            commandBuffer.present(drawable)
+            
+            commandBuffer.addCompletedHandler {[weak self] _ in
+                self?.semaphore.signal()
+            }
+            commandBuffer.commit()
+        }
+    }
+}
