aa1ed63b83e30e271df789f84149a7d82aaa8f33
tokyoyoshida <yoshidaforpublic@gmail.com>
2021-09-17 17:34:29 +0900

    add vc

--- MetalExamples/ContentView.swift
+++ MetalExamples/ContentView.swift
@@ -12,41 +12,51 @@ struct ContentView: View {
         NavigationView {
             List {
                 Section(header: Text("Samples")) {
-                    NavigationLink(destination: SimpleView()) {
-                        Text("Simple")
-                            .padding()
+                    Section(header: Text("Simple")) {
+                        NavigationLink(destination: SimpleView()) {
+                            Text("Simple")
+                                .padding()
+                        }
+                        NavigationLink(destination: DrawTextureView()) {
+                            Text("Draw Texture")
+                                .padding()
+                        }
+                        NavigationLink(destination: SimpleShaderViewv()) {
+                            Text("Simple Shader")
+                                .padding()
+                        }
+                        NavigationLink(destination: DrawToCAMetalLayerView()) {
+                            Text("Draw to CAMetalLayer")
+                                .padding()
+                        }
+                        NavigationLink(destination: VideoEffectView()) {
+                            Text("Video Effect")
+                                .padding()
+                        }
+                        NavigationLink(destination: DrawIndexedPrimitiveView()) {
+                            Text("Draw Indexed Primitive")
+                                .padding()
+                        }
+                        NavigationLink(destination: MultiPassRenderingView()) {
+                            Text("Multi Pass Rendering")
+                                .padding()
+                        }
                     }
-                    NavigationLink(destination: DrawTextureView()) {
-                        Text("Draw Texture")
-                            .padding()
+                    Section(header: Text("Particle")) {
+                        NavigationLink(destination: ParticleView()) {
+                            Text("Particle")
+                                .padding()
+                        }
+                        NavigationLink(destination: TripleBufferingView()) {
+                            Text("TripleBuffering")
+                                .padding()
+                        }
                     }
-                    NavigationLink(destination: SimpleShaderViewv()) {
-                        Text("Simple Shader")
-                            .padding()
-                    }
-                    NavigationLink(destination: DrawToCAMetalLayerView()) {
-                        Text("Draw to CAMetalLayer")
-                            .padding()
-                    }
-                    NavigationLink(destination: ParticleView()) {
-                        Text("Particle")
-                            .padding()
-                    }
-                    NavigationLink(destination: TripleBufferingView()) {
-                        Text("TripleBuffering")
-                            .padding()
-                    }
-                    NavigationLink(destination: VideoEffectView()) {
-                        Text("Video Effect")
-                            .padding()
-                    }
-                    NavigationLink(destination: DrawIndexedPrimitiveView()) {
-                        Text("Draw Indexed Primitive")
-                            .padding()
-                    }
-                    NavigationLink(destination: Draw3DView()) {
-                        Text("Draw 3D by Depth Stencil")
-                            .padding()
+                    Section(header: Text("3D")) {
+                        NavigationLink(destination: Draw3DView()) {
+                            Text("Draw 3D by Depth Stencil")
+                                .padding()
+                        }
                     }
                     Section(header: Text("MSL Shader Examples")) {
                         NavigationLink(destination: SimpleShapeView()) {
